f9d35650a6504263f71212b4bedf6c59a151df51
MichalEder <edermichal.eder@gmail.com>
2023-03-26 15:54:29 +0200

    zapsana hlavicka

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+1st_Project_ENGETO: první projekt do Engeto Online Python Akademie
+author: Michal Eder
+email: edermichal.eder@gmail.com
+discord: Petr Svetr#4490
+'''
+
+######################################################################################################################
+
+
