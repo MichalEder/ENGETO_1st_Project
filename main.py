444a8c1537a4b9a12fe1d5ac24e0db874a30185d
MichalEder <edermichal.eder@gmail.com>
2023-03-26 20:43:39 +0200

    pridano rozdeleni a vycisteni textu

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#### HLAVICKA ####
 '''
 1st_Project_ENGETO: první projekt do Engeto Online Python Akademie
 author: Michal Eder
 email: edermichal.eder@gmail.com
 discord: Petr Svetr#4490
 '''
+#### /HLAVICKA #####
+#### ZADANI ####
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -35,23 +38,47 @@ users = ["bob", "ann", "mike", "liz"]
 passwords = ["123", "pass123", "password123", "pass123"]
 users_passwords = dict(zip(users, passwords))
 separator = "*"*60
-######################################################################################################################
+#### /ZADANI ####
+#### LOGIN ####
 print(separator)
 print(f'LOGIN'.center(60))
 print('Please, enter your login credentials'.center(60))
 print(separator)
-user = input('User:')
-password = input('Password:')
+user = input('User: ')
+password = input('Password: ')
+#### /LOGIN ####
+#### LOGIN-CHECK ####
 print(separator)
 if user in users_passwords.keys() and password == users_passwords[user]:
-    print(f'Welcome to app, {user}')
-    print('We have three texts to analzye')
+    pass
 else:
     print('INVALID LOGIN CREDENTIALS'.center(60))
-    quit
+    quit()
+##### /LOGIN-CHECK #####
+print(f'Welcome to app, {user}')
+print('We have three texts to analyze')
+print(separator)
+#### TEXT-CHOICE ####
+text_choice = int(input('Enter number 1-3 to choose text for analysis: '))
+if text_choice not in range(1,3):
+    print('INVALID CHOICE')
+    quit()
+else:
+    pass
+print(separator)
+#### /TEXT-CHOICE ####
+analyzed_text = (TEXTS[text_choice-1]).split()
+analyzed_text_clear = [word.strip('.,?!:') for word in analyzed_text]
+#print(analyzed_text_clear)
+
+#počet slov
+pocet_slov = 
+#počet slov začínajících velkým písmenem,
+#počet slov psaných velkými písmeny,
+#počet slov psaných malými písmeny,
+#počet čísel (ne cifer),
+#sumu všech čísel (ne cifer) v textu
 
 
-
-#volba_textu
 #analyza_textu
 
