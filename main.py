31ca8f30e8273dcf580aaded5a4c5f68269a041c
MichalEder <edermichal.eder@gmail.com>
2023-03-27 19:22:24 +0200

    Uprava formatui, doplnen: vystup vykresleni grafu

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#### HLAVICKA ####
+#### HEAD ####
 '''
 1st_Project_ENGETO: první projekt do Engeto Online Python Akademie
 author: Michal Eder
 email: edermichal.eder@gmail.com
-discord: Petr Svetr#4490
+discord: Michal Eder#2018
 '''
 #### /HLAVICKA #####
-#### ZADANI ####
+#### ENTRY ####
 TEXTS = ['''
 Situated about 10 miles west of Kemmerer,
 Fossil Butte is a ruggedly impressive
@@ -37,8 +37,8 @@ garpike and stingray are also present.'''
 users = ["bob", "ann", "mike", "liz"]
 passwords = ["123", "pass123", "password123", "pass123"]
 users_passwords = dict(zip(users, passwords))
-separator = "*"*60
-#### /ZADANI ####
+separator = "-"*60
+#### /ENTRY ####
 #### LOGIN ####
 print(separator)
 print(f'LOGIN'.center(60))
@@ -52,7 +52,9 @@ print(separator)
 if user in users_passwords.keys() and password == users_passwords[user]:
     pass
 else:
-    print('INVALID LOGIN CREDENTIALS'.center(60))
+    print('INVALID USER OR PASSWORD'.center(60))
+    print('TERMINATING PROGRAM'.center(60))
+    input()
     quit()
 ##### /LOGIN-CHECK #####
 print(f'Welcome to app, {user}')
@@ -60,25 +62,60 @@ print('We have three texts to analyze')
 print(separator)
 #### TEXT-CHOICE ####
 text_choice = int(input('Enter number 1-3 to choose text for analysis: '))
-if text_choice not in range(1,3):
-    print('INVALID CHOICE')
-    quit()
-else:
-    pass
+if text_choice not in range(1,4):
+    print('INVALID CHOICE'.center(60))
+    print('TERMINATING PROGRAM'.center(60))
+    input()
+    quit()   
 print(separator)
 #### /TEXT-CHOICE ####
 analyzed_text = (TEXTS[text_choice-1]).split()
 analyzed_text_clear = [word.strip('.,?!:') for word in analyzed_text]
 #print(analyzed_text_clear)
+#WORD COUNT
+word_len_category = dict()
+title_case = 0
+upper_case = 0
+lower_case = 0
+numeric = 0
+numeric_sum = 0
+for word in analyzed_text_clear:
+    if len(word) in word_len_category.keys():
+        word_len_category[len(word)] += 1
+    else:
+        word_len_category[len(word)] = 1
+    if word.isalpha():
+        if word.istitle():
+            title_case += 1
+        elif word.islower():
+            lower_case += 1
+        elif word.isupper():
+            upper_case += 1
+            title_case += 1
+    elif word.isnumeric():
+        numeric += 1
+        numeric_sum += int(word)
+#/WORD COUNT
+#COUNT OUTPUT
+print(f"""
+There are {len(analyzed_text_clear)} words in the selected text.
+There are {title_case} titlecase words.
+There are {upper_case} uppercase words.
+There are {lower_case} lowercase words.
+There are {numeric} numeric strings.
+The sum of all numbers is {numeric_sum}.
+""")
+#/COUNT OUTPUT
 
-#počet slov
-pocet_slov = 
-#počet slov začínajících velkým písmenem,
-#počet slov psaných velkými písmeny,
-#počet slov psaných malými písmeny,
-#počet čísel (ne cifer),
-#sumu všech čísel (ne cifer) v textu
-
-
-#analyza_textu
-
+wlc_sorted = dict(sorted(word_len_category.items()))
+print("LEN", "|".rjust(1),"OCCURENCES".center(20),"|".ljust(2),"NR.")
+print("-"*35)
+for key,value in wlc_sorted.items():
+    print(
+        (str(key).rjust(2)),
+        ("|".rjust(2)),
+        ("*"*value).ljust(20),
+        ("|".ljust(1)),
+        (str(value).rjust(3))
+        )
+input()
